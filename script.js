af82e08d16829666c50a32b7df28097132114c2a
Marco Peters <m.peters646@gmail.com>
2021-03-25 19:24:44 +0100

    Added new excersise files

---
+++ script.js
@@ -0,0 +1,14 @@
+const huiswerkMaken = (vak, callback) => {
+  console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken.`);
+  document.getElementById('para1').innerHTML = `Ok, ok, ik ga nu mijn ${vak} huiswerk maken.`;
+  callback();
+};
+
+const klaarMetHuiswerk = () => {
+  setTimeout(() => {
+    console.log(`Kijk Paps/Mams, ik ben klaar met mijn huiswerk.`);
+    document.getElementById('para2').innerHTML = `Kijk Paps/Mams, ik ben klaar met mijn huiswerk.`;
+  }, 5000);
+};
+
+huiswerkMaken('Wiskunde', klaarMetHuiswerk);
